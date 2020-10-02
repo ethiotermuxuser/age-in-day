2a6e1c96629a45e28741e59cba2ce21d915e4865
programmer-ethio <programmernegn@gmail.com>
2020-10-02 13:37:10 +0300

    first commit

---
+++ mm.js
@@ -0,0 +1,16 @@
+function ageindays (){
+    var brithday = prompt('how old are you or your friend')
+    var brithdays = 'you ' + 'are ' + brithday * 365 + ' days' + ' years old'
+    document.getElementById('flex-box-result').innerHTML = brithdays
+    }
+      function reset() {
+        document.getElementById('flex-box-result').remove();
+    } 
+function ageindays (){
+     var brithdayy = prompt('how old are you or your friend')
+     var brithdayss = 'you ' + 'are ' + brithdayy * 365 + ' days' + ' years old'
+     document.getElementById('flex-box-resultt').innerHTML = brithdayss
+     }
+function reset() {
+ document.getElementById('flex-box-resultt').remove();
+}
